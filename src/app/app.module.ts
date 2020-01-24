5494d73f30b6cf172d537c64e0385390af4a16e1
DBNubs <iam@jasonblanda.com>
2020-01-23 20:36:38 -0500

    With dependency injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserDemoComponent } from './user-demo/user-demo.component';
+import { UserDemoInjectorComponent } from './user-demo/user-demo.injector.component';
+
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDemoComponent,
+    UserDemoInjectorComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user: any;
+
+  setUser(newUser) {
+    this.user = newUser;
+  }
+
+  getUser(): any {
+    return this.user;
+  }
+
+}
